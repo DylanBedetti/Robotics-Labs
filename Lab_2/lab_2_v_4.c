85b219f08bf0fbaad185dfeacacecb49e82d3dfa
DylanBedetti <dylanbedetti@gmail.com>
2019-08-24 18:04:20 +0800

    creating final lab2 file

--- Lab_2/lab_2_v_4.c
+++ Lab_2/lab_2_v_4.c
@@ -21,12 +21,14 @@
 
 int x; int y; int phi = 0; // VWGetPosition variables
 float x_val = 0; float y_val = 0; // recording spline x and y coordinates
-float sum = 0;
+float sum = 0; // used for spline calculation
 int steps = 15; // number of discrete steps of spline!
+float phi_error; float distance; 
+int speed = 50; // base speed
 
-float phi_error; float phi_to_point; float distance; 
-int speed = 30; float push = 1; 
-float turn_rate;
+// txt file stuff
+int points[20][2];
+int i = 0;
 
 float hermite[4][4] = {
     {2, -2, 1, 1},
@@ -58,62 +60,22 @@ float vector[2][1] = {
     {0}
 };
 
-
-void follow_curve(int x_p, int y_p){
-    printf("\n\n ------ FOLLOWING CURVE ------- \n\n");
-    VWGetPosition(&x, &y, &phi);
-    phi_error = atan2(y_p - y,x_p - x)*180/pi - phi;
-    distance = sqrt((y_p - y)*(y_p - y) + (x_p - x)*(x_p - x) );
-
-
-    while (distance > 2){
-        // need to consider when phi error is negative
-        turn_rate = fmin(abs(phi_error)*push*fmax(-distance + 5, 1), 10);
-        // should speed be altered by distance?
-        
-        
-
-        if(phi_error > 1){
-            // turn left
-            MOTORDrive(1, speed - turn_rate);
-            MOTORDrive(2, speed + turn_rate);
-        }
-        else if(phi_error < -1){
-            // turn right
-            MOTORDrive(1, speed + turn_rate);
-            MOTORDrive(2, speed - turn_rate);
-        }
-        else{
-            VWStraight(10, 30);
-        }
-
-        VWGetPosition(&x, &y, &phi);
-        distance = sqrt((y_p - y)*(y_p - y) + (x_p - x)*(x_p - x) );
-        phi_error = atan2(y_p - y,x_p - x)*180/pi - phi;
-
-        printf("\r distance: %f, phi_error: %f, turn rate: %f, x: %d, y: %d", distance, phi_error,turn_rate,x, y);
-        fflush(stdout);
-    }
-    MOTORDrive(1, 0);
-    MOTORDrive(2, 0);
-
-    VWGetPosition(&x, &y, &phi);
-    printf("\n\nx: %d, y: %d, phi: %d, phi error: %f, distance: %f\n\n", x, y, phi, phi_error , distance);
-}
-
 void follow_points(int x_p, int y_p){
     // control function for each point on spline 
     printf("\n------- FOLLOWING POINTS ----------\n");
 
+    // Updating position, phi error and distance
     VWGetPosition(&x, &y, &phi);
     phi_error = atan2(y_p - y,x_p - x)*180/pi - phi;
     distance = sqrt((y_p - y)*(y_p - y) + (x_p - x)*(x_p - x) );
 
     printf("START: phi error: %f\tdistance: %f\tphi: %d\tx: %d\ty: %d\n", phi_error, distance, phi, x, y);
 
+    // using basic curve function as control
     VWCurve(distance, phi_error, speed);
     VWWait();
 
+    // again updating position, phi error and distance
     VWGetPosition(&x, &y, &phi);
     phi_error = atan2(y_p - y,x_p - x)*180/pi - phi;
     distance = sqrt((y_p - y)*(y_p - y) + (x_p - x)*(x_p - x) );
@@ -163,7 +125,15 @@ void spline(){
     }
 }
 
-
+void print_array(){
+    // function for printing out pos array
+    for (int k = 0; k < 4; k++){
+            for (int j = 0; j < 2; j++){
+                printf("%.0f\t", pos[k][j]);
+            }
+            printf("\n");
+        }
+}
 
 int main() {
     // set robot position
@@ -176,7 +146,41 @@ int main() {
     printf("\n\n STARTING PROGRAM");
     printf("\nx: %d, y: %d, phi: %d\n\n", x, y, phi);
 
+    // looping through text file and calling spline();
+    FILE* file = fopen("way.txt", "r");
+    while (fscanf(file,"%d %d", &points[i][0], &points[i][1]) == 2) {
+        printf("\nPoint: %d\n", i);
+        if (i == 0){
+            // printf("x:%d y:%d\n", points[i][0], points[i][1]);
+            pos[1][0] = points[i][0];
+            pos[1][1] = points[i][1];
+            print_array();
+            spline();
+            
+        } else{
+    	    // printf("x:%d y:%d\n", points[i][0] - points[i-1][0], points[i][1]-points[i-1][1]);
+            pos[0][0] = points[i-1][0];
+            pos[0][1] = points[i-1][1];
+            
+            pos[1][0] = points[i][0];
+            pos[1][1] = points[i][1];
+
+            print_array();
+            spline();
+        }
+	i++;
+    }
+    pos[0][0] = pos[1][0];
+    pos[0][1] = pos[1][1];
+
+    pos[1][0] = 0;
+    pos[1][1] = 0;
+    
+    printf("\nPoint: %d\n", i);
+    print_array();
 
     spline();
+
+    fclose(file);
     return 0;
 }
--- Lab_2/readfile.c
+++ Lab_2/readfile.c
@@ -1,13 +1,55 @@
 #include <stdio.h>
 
+float pos[4][2] = {
+    {0, 0}, // P1 -> Start Point (x,y)
+    {400, 1000}, // P2 -> End Point (x,y)
+    {0, 0}, // T1 -> Start Tangent (x,y)
+    {0, 0} // T2 -> End Tangent (x,y)
+} ;
+
+void print_array(){
+    for (int k = 0; k < 4; k++){
+            for (int j = 0; j < 2; j++){
+                printf("%.0f\t", pos[k][j]);
+            }
+            printf("\n");
+        }
+}
+
 int main() {
     FILE* file = fopen("way.txt", "r");
     int points[20][2];
     int i = 0;
     while (fscanf(file,"%d %d", &points[i][0], &points[i][1]) == 2) {
-    	printf("x:%d y:%d\n", points[i][0], points[i][1]);
+        printf("\nPoint: %d\n", i);
+        if (i == 0){
+            // printf("x:%d y:%d\n", points[i][0], points[i][1]);
+            pos[1][0] = points[i][0];
+            pos[1][1] = points[i][1];
+            print_array();
+            
+        } else{
+    	    // printf("x:%d y:%d\n", points[i][0] - points[i-1][0], points[i][1]-points[i-1][1]);
+            pos[0][0] = points[i-1][0];
+            pos[0][1] = points[i-1][1];
+            
+            pos[1][0] = points[i][0];
+            pos[1][1] = points[i][1];
+
+            print_array();
+        }
 	i++;
-	}
+    }
+    pos[0][0] = pos[1][0];
+    pos[0][1] = pos[1][1];
+
+    pos[1][0] = 0;
+    pos[1][1] = 0;
+    
+    printf("\nPoint: %d\n", i);
+    print_array();
+
+
     fclose(file);
 }
 
