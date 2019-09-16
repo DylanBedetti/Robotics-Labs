850ae34cf3fdd9136ff6436ca057eb158e0e0995
DylanBedetti <dylanbedetti@gmail.com>
2019-09-16 22:15:34 +0800

    ya

--- Lab_4/lab_4_v_1.c
+++ Lab_4/lab_4_v_1.c
@@ -3,58 +3,133 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdbool.h>
 
 #define LINE_MAX 255
-#define DEPTH_LIMIT 4
+#define DEPTH_LIMIT 8 //in terms of pixels!
 #define MAX_QUADS 256
 
 BYTE* img;
 BYTE img2D[128][128]; 
+char* filename = "corner.pbm";
 
 typedef struct Quadbox {
     int xcoord;
     int ycoord;
     int width;
-} Quadbox;
-
+} 
+Quadbox;
 Quadbox blocked_quads[MAX_QUADS];
 Quadbox free_quads[MAX_QUADS];
 
 
 void vector2matrix(){
-    int k = 0; 
     for(int i = 0; i <128 ;i++){ 
         for(int j = 0; j <128 ; j++){
             img2D[i][j]= img[(i*128+j)];
-            k++;
+			// printf("%d", img2D[i][j]);
         }
+	// printf("\n");
     }
 }
 
-bool checkEmpty(int x, int y, int depth){
-	for (int j = 0; j < 64 / pow(depth, 2); j++){
-		for (int i = 0; i < 64 / pow(depth, 2); i++){
-
+int isAllEmpty(int x, int y, int depth){
+	printf("\n\nISALLEMPYT DEPTH: %d\n", depth);
+	int sum = 0;
+	for (int i = x; i <(64 / pow(2, depth)) + x; i++){
+		for (int j = y; j < (64 / pow(2, depth)) + y; j++){
+			if (img2D[i][j] == 1){
+				sum = 1;
+			}
 		}
+	}
+	if (sum > 0){
+		return false;
+	} else {
+		return true;
 	}
 }
 
-void quadtree(int x, int y, int width, int depth_limit) {
-	if (depth_limit > 0) {
+bool isAllFull(int x, int y, int depth){
+	int sum = 0;
+	for (int i = x; i < (64 / pow(2, depth)) + x; i++){
+		for (int j = y; j < (64 / pow(2, depth)) + y; j++){
+			if (img2D[i][j] == 1){
+				sum = 1;
+			}
+		}
+	}
+	if (sum ==  1){
+		return true;
+	} else {
+		return false;
+	}
+}
+
+
+int block_quad_number = 0;
+int free_quad_number = 0;
+int count = 0;
+void quadtree(int x, int y, int width, int depth) {
+	count++; printf("count: %d\n", count);
+	if (width >= DEPTH_LIMIT) {
+		if (isAllEmpty(x, y, depth)){
+			printf("all empty\n");
+			free_quads[free_quad_number].xcoord = x;
+            free_quads[free_quad_number].ycoord = y;
+            free_quads[free_quad_number].width = width;
+			free_quad_number += 1;
+		} else if (isAllFull(x, y, width)){
+			printf("all full\n");
+			blocked_quads[block_quad_number].xcoord = x;
+            blocked_quads[block_quad_number].ycoord = y;
+            blocked_quads[block_quad_number].width = width;
+			block_quad_number++; 
+		} else { // dive further
+			int width2 = width/2;
+			int depth2 = depth + 1;
+            quadtree(x, y, width2, depth2);
+            quadtree(x + width2, y, width2, depth2);
+            quadtree(x, y + width2, width2, depth2);
+            quadtree(x + width2, y + width2, width2, depth2);
+		}
+	} else { // depth limit reached
+		if (isAllEmpty(x, y, depth)){
+			printf("all empty\n");
+			free_quads[free_quad_number].xcoord = x;
+            free_quads[free_quad_number].ycoord = y;
+            free_quads[free_quad_number].width = width;
+			free_quad_number += 1;
+		} else {
+			printf("all full\n");
+			blocked_quads[block_quad_number].xcoord = x;
+            blocked_quads[block_quad_number].ycoord = y;
+            blocked_quads[block_quad_number].width = width;
+			block_quad_number++; 
+		}
+	}
 }           
 
 
 int main(){
-    char* filename = "corner.pbm";
     read_pbm(filename, &img);
-    printf("%d\n", img[0]);
+	printf("starting!\n");
 
-    LCDImageStart(0, 0, 128, 128);
-    LCDImageBinary(img);
+    // LCDImageStart(0, 0, 128, 128);
+    // LCDImageBinary(img);
 
-    for (int t = 0; t < 100; t++){
-        usleep(100000);
-        printf("%d\n", t);
-    }
+    // for (int t = 0; t < 100; t++){
+    //     usleep(100000);
+    //     printf("%d\n", t);
+    // }
+	vector2matrix();
+	quadtree(0, 0, 128, 0);
+
+	printf("blocked: %d", block_quad_number);
+	printf("free: %d", free_quad_number);
+
+	// printf("%d\n", isAllEmpty(64, 100, 2));
+	// printf("%d\n", isAllFull(64, 0, 2));
+
     return 0;
 }
