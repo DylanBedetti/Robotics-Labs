3f7f01f5256c0dc7115432dc527f0087efe4b2f7
DylanBedetti <dylanbedetti@gmail.com>
2019-09-19 21:08:35 +0800

    bushfire

--- Lab_5/lab_5_v_1.c
+++ Lab_5/lab_5_v_1.c
@@ -13,8 +13,9 @@ ORANGE, SILVER, LIGHTGRAY, DARKGRAY, NAVY, CYAN, TEAL, MAGENTA, PURPLE, MAROON, 
 */
 int colours[] = {0 ,RED, GREEN, BLUE, WHITE, GRAY, ORANGE, SILVER, LIGHTGRAY, DARKGRAY, NAVY, CYAN, TEAL, MAGENTA, PURPLE, MAROON, YELLOW, OLIVE};
 BYTE* img;
-BYTE img2D[128][128] = {0}; 
-int colour_array[128][128];
+BYTE img2D[128][128]; 
+BYTE img2D_temp[128][128];
+int colour_array[128][128] = {0};
 char* filename = "rectangles.pbm";
 
 void vectorToMatrix(){
@@ -25,15 +26,12 @@ void vectorToMatrix(){
     }
 }
 
-void printOutArray(int c){
+void printOutArray(){
     for(int i = 0; i < 128; i++){
         for(int j = 0; j < 128; j++){
-            // printf("%d", img2D[i][j]);
-            if (img2D[i][j] == 1){
-                LCDPixel(j, i, c);
-            }
+            printf("%d", img2D[i][j]);
         }
-    // printf("\n");
+    printf("\n");
 }
 }
 
@@ -41,39 +39,87 @@ void printOutColourArray(){
     for(int r = 0; r < 128; r++){
         for(int c = 0; c < 128; c++){
             // printf("%d", img2D[i][j]);
-            LCDPixel(c, r, colour_array[r][c]);
+            LCDPixel(c, r, colours[colour_array[r][c]]);
         }
     // printf("\n");
+    }
 }
+
+int check_zeros(){
+    for(int i = 0; i < 128; i++){
+        for(int j = 0; j < 128; j++){
+            if (img2D[i][j] == 0){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+void copyArrays(int k){
+    for(int i = 0; i < 128; i++){
+        for(int j = 0; j < 128; j++){
+            if (k == 1){
+                img2D_temp[i][j] = img2D[i][j];
+            } else{
+                img2D[i][j] = img2D[i][j];
+            }
+
+        }
+    }
 }
 
 
 void brushfire(){
-    //label each object independently
-    int object_num = 1;
+    printf("doing brushfire!\n");
+    printf("lablling objects\n");
+    //label each object independently and assigning edges to 2
+    int object_num = 0;
     for (int r = 0; r < 128; r ++){
         for (int c = 0; c < 128; c++){
             if (img2D[r][c] == 1){
-                if(colour_array[r][c - 1] != 0 || colour_array[r - 1][c] != 0){
-                    colour_array[r][c] = object_num;
+                if(colour_array[r][c - 1] != 0){
+                    colour_array[r][c] = colour_array[r][c-1];
+                    // printf("r: %d, c: %d\n", r, c);
+                } else if(colour_array[r - 1][c] != 0){
+                    colour_array[r][c] = colour_array[r-1][c];
+                    // printf("r: %d, c: %d\n", r, c);
                 }
                 else{
+                    // printf("new object num: %d, r: %d, c: %d\n", object_num, r, c);
                     object_num++;
                     colour_array[r][c] = object_num;
                 }
+            }
+            // if (r == 0 || r == 127 || c == 0 || c == 127){
+            //     img2D[r][c] = 2;
+            // }
+        }
+    }
 
+    printOutArray();
+    getchar();
 
-                // if ((img2D[r][c + 1] == 1) || (img2D[r - 1][c + 1] == 1)){
-                //     colour_array[r][c] = colours[object_num];
-                //     printf("r: %d, c: %d\n", r, c);
-                // }
-                // else{
-                //     printf("new object num: %d, r: %d, c: %d\n", object_num, r, c);
-                //     object_num++;
-                //     colour_array[r][c] = object_num;
-                // }
+    // img2D_temp = img2D;
+    copyArrays(1);
+
+    printf("Starting numbering\n");
+    // looping through
+    int contains_zeros = 0;
+    for (int i = 2; contains_zeros == 0; i++){
+        for (int r = 1; r < 127; r++){
+            for (int c = 1; c < 127; c++){
+                if ((img2D[r + 1][c] != 0 || img2D[r - 1][c] != 0 || img2D[r][c + 1] != 0 || img2D[r][c - 1] != 0) && (img2D[r][c] == 0)){
+                    img2D_temp[r][c] = i;
+                }
             }
         }
+        printOutArray();
+        getchar(); 
+        contains_zeros = check_zeros();
+        printf("contains zeros: %d\n", contains_zeros);
+        // img2D = img2D_temp;
+        copyArrays(0);
     }
 }
 
@@ -87,19 +133,22 @@ int main(){
 	printf("starting program!\n\n");
 
 	// starting LCD
-    LCDImageStart(0, 0, 128, 128);
-    LCDImageBinary(img);
+    // LCDImageStart(0, 0, 128, 128);
+    // LCDImageBinary(img);
 
     vectorToMatrix();
     // printOutArray(GREEN);
     // LCDSetPrintf(15, 0, "HELLOO");
 
-    // brushfire();
+    brushfire();
     // printOutColourArray();
 
-    printf("%d\n",img2D[13][82]);
-    printf("%d\n",img2D[13][83]);
-    printf("%d\n",img2D[13][84]);
+    // for(int i = 0; i < 128; i++){
+    //     for(int j = 0; j < 128; j++){
+    //         printf("%d", img2D[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 
     printf("Press enter to end program\n");
     getchar(); 
